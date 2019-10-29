3955cdc1c9b5befbe867513bc0b3d7c803c24ff4
Thomas Gilmore <tomtitans@aol.com>
2019-10-29 15:52:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-function getFirstSelector(selector)
-{
-  document.querySelector(selector)
-}
+function getFirstSelector(selelctor){
+   document.querySelector(selector)
+   }
 
-function nestedTarget()
-{
-  target.addEventListener('click', nestedTarget())
-}
+  function nestedTarget(){
+   document.querySelector('#nested .target')
+ }
 
-function deepestChild()
-{
+  function increaseRankBy(n){
+ var list = document.querySelectorAll('ul.ranked-list li')
+ for (let i = 0; i < list.length;i++{
+   list[i].innerHTML= parseInt(list[i].innerHTML, 10) + n;
+   }
+ }
 
-}
-
-function increaseRankBy(n)
-{
-
-}
+  function deepestChild() {
+   const list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+   for (let i=0, 1=list.length; i<1; i++) {
+     list[i].innerHTML = (i+1).toString()
+   }
